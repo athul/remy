62a21fbecc84a880a6cf2cc5a138cd9ad08fb8cc
athul <athul8720@gmail.com>
2020-02-04 23:10:42 +0530

    Added Rendering Updates

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
-const url = "https://api.github.com/repos/athul/shelby/releases"
+let repVal=document.getElementById("search-input");
+let term=repVal.value;
+console.log(term)
+//let url=
+axios.get('https://api.github.com/repos/athul/shelby/releases')
+    .then(response => {
+        return response.data;
+    }).catch(error => {
+        console.log(error);
+    }).then(data => {
+        const asset_downloads = data.map(release => {
+            return {
+                name: release.tag_name,
+                assets: release.assets.map(({ name, download_count }) => ({ name, download_count }))
+            }
+        });
+        this.results = asset_downloads;
 
-const vm = new Vue({
-    el: '#app',
-    data: {
-        results: [],
-    },
-    mounted() {
-        axios.get('https://api.github.com/repos/athul/shelby/releases')
-            .then(response => {
-                return response.data;
-            }).then(data => {
-                const asset_downloads = data.map(release => {
-                    return {
-                        name: release.tag_name,
-                        assets: release.assets.map(({ name, download_count }) => ({ name, download_count }))
-                    }
-                });
-                return asset_downloads;
-            }).then(total_downloads => {
-                this.results = JSON.stringify(total_downloads, null, 2);
-                console.log(this.results)
-            }).catch(error => {
-                console.log(error);
+        let htmlString = [];
+        asset_downloads.forEach(item => {
+            let assets = item.assets.map(asset => {
+                return (
+                    `<div class='card asset'>
+                        Name of Asset: <strong>${asset.name}</strong><br>
+                        <p class="font-black">Downloads: ${asset.download_count}</p>
+                    </div>`
+                )
             })
-    }
-});
-console.log(vm)
-
+            htmlString.push(
+                `<div class="px-6 py-6 card">
+                            <div class="font-bold text-xl mb-2"><u>Tag: ${item.name}</u></div>
+                            <div class='assets'>
+                                ${assets.join('')}
+                            </div>
+                        </div>`
+            )
+        })
+        document.querySelector("#main").innerHTML = htmlString.join('');
+        return asset_downloads;
+    })
 
